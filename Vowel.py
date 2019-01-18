6a960b14861c2b675778481a346ec4c7d418a996
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-01-18 09:43:14 +0530

    Update Vowel.py

--- Vowel.py
+++ Vowel.py
@@ -1,9 +1,6 @@
 a=input()
 l=['a','e','i','o','u']
-if a.isalpha():
-    if any(i in l for i in a):
-        print("yes")
-    else:
-      print("no")
+if any(i in l for i in a):
+  print("yes")
 else:
   print("no")
