5fa8453c41e61ff39275754bdf92b75b3797504c
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-01-30 16:10:38 +0530

    Update Camel_case.py

--- Camel_case.py
+++ Camel_case.py
@@ -1,13 +1,4 @@
 s=input()
-t=""
-if s[0].isupper():
-  pass
-else:
-  t=t+s[0].upper()
-for i in range(1,len(s)):
-  if s[i]==" ":
-    t=t+s[i]
-    t=t+s[i+1].upper()
-  else:
-    t=t+s[i]
-print(t)
+s=s.split(" ")
+for i in s:
+  print(i[0].upper()+i[1:].lower(),end=" ")
