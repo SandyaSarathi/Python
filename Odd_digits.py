6fa264454e52b8f3206349ee6760930d1529c2ce
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-02-04 16:04:48 +0530

    Update Odd_digits.py

--- Odd_digits.py
+++ Odd_digits.py
@@ -1,9 +1,9 @@
 n=input()
 n1=[]
+s=""
 for i in n:
   n1.append(int(i))
-for i in range(0,len(n1)-1):
-  if n1[i]%2!=0:
-    print(n1[i],end=" ")
-if n1[len(n1)-1]%2!=0:
-  print(n1[len(n1)-1])
+for i in range(0,len(n1)):
+  if (n1[i]%2!=0):
+    s=s+str(n1[i])+" "
+print(s.strip())
