a40f345a6691650d1ee8add518290e498c2e9c66
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-02-11 16:02:11 +0530

    Create 86.py

---
+++ 86.py
@@ -0,0 +1,16 @@
+s=input()
+l=list(s)
+l1=[]
+f=0
+for i in l:
+    l1.append(i)
+    l.remove(i)
+    print(l)
+    if i in l:
+        f=1
+        break
+if f==1:
+    print("no")
+else:
+    print("yes")
+    
