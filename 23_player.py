1d4aea6969bf24f7c5e105f72af4aeea0f44bbb4
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-02-18 19:50:26 +0530

    Create 23_player.py

---
+++ 23_player.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+l=[int(i) for i in input().split()]
+l1=[int(i) for i in input().split()]
+l2=[]
+for i in l1:
+  l.append(i)
+  l2.append(max(l))
+for i in range(0,len(l2)-1):
+  print(l2[i],end=" ")
+print(l2[len(l2)-1])
