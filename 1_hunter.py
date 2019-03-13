2008fa0e3fbdf1b46f81eb688c780064d4bcd76a
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-03-13 22:47:43 +0530

    Create 1_hunter.py

---
+++ 1_hunter.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=[int(i) for i in input().split()]
+l1=[]
+for i in range(0,len(l)):
+  for j in range(i+1,len(l)):
+    if l[i]==l[j]:
+      if l[i] not in l1:
+        l1.append(l[i])
+      break
+l1=sorted(l1)
+for i in range(0,len(l1)-1):
+  print(l1[i],end=" ")
+print(l1[len(l1)-1])
