4151ab87be7f3ce94344004967052c9530f6253f
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-03-13 22:07:17 +0530

    Create 37_player.py

---
+++ 37_player.py
@@ -0,0 +1,12 @@
+#sandya
+n = int(input(""))
+lis = []
+for i in range(n):
+    temp = list(map(int,input().split()))
+    lis.append(temp)
+lis.sort(key = lambda x: x[0])
+c =0
+for i in range(len(lis)-1):
+    if(lis[i][0] < lis[i+1][0]):
+        c+=1
+print(c)
