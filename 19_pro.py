0f75d6626bf227b4b05fe5d455533a485714f394
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-03-14 19:05:46 +0530

    Update 19_pro.py

--- 19_pro.py
+++ 19_pro.py
@@ -3,5 +3,8 @@ l=[int(i) for i in input().split()]
 l1=[int(i) for i in input().split()]
 l2=[int(i) for i in input().split()]
 l3=l1+l2+l
-print(sorted(l3))
+l3=sorted(l3)
+for i in range(0,len(l3)-1):
+  print(l3[i],end=" ")
+print(l3[len(l3)-1])
 
