2d7a5790d8d5f37d7441610b5035235fa4440579
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-03-14 22:33:18 +0530

    Update 57_hunter.py

--- 57_hunter.py
+++ 57_hunter.py
@@ -1,11 +1,9 @@
 n=int(input())
 l=[int(i) for i in input().split()]
-for i in range(0,len(l)):
-  m=l[i]
-  del(l[i])
-  if m not in l:
-    print(m)
-    break
+i=0
+for i in l:
+  if l.count(i)!=2:
+    print(i)
 
     
 
