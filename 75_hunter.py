8315aeab4030da6154287c1defa29777063e97ec
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-03-14 22:58:26 +0530

    Create 75_hunter.py

---
+++ 75_hunter.py
@@ -0,0 +1,13 @@
+n=int(input())
+l1=[]
+l=[int(i) for i in input().split()]
+for i in range(0,len(l)-1):
+  if l[i]>l[i+1]:
+    l1.append(l[i+1])
+    print(l1)
+  else:
+    l1.append(-1)
+print(*l1,end=" ")
+print(-1)
+
+
