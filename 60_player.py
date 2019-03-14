49c59e6dc04e56bc877099eaa673a4bd65b5d1d9
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-03-14 23:55:44 +0530

    Create 60_player.py

---
+++ 60_player.py
@@ -0,0 +1,11 @@
+#san
+n,k=map(str,input().split(" "))
+count=0
+for i in range(0,len(n)):
+	for j in range(i,len(k)):
+		if n[i]==k[j]:
+			count=count+1
+if count>=1:
+	print("yes")
+else:
+	print("no")
