78c15864dca75464a44d8d13af6ccd848bf85050
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-03-14 19:40:12 +0530

    Update 19_pro.py

--- 19_pro.py
+++ 19_pro.py
@@ -1,10 +1,14 @@
 n=int(input())
+l=[]
+t=[]
+l1=[]
 l=[int(i) for i in input().split()]
-l1=[int(i) for i in input().split()]
-l2=[int(i) for i in input().split()]
-l3=l1+l2+l
-l3=sorted(l3)
-for i in range(0,len(l3)-1):
-  print(l3[i],end=" ")
-print(l3[len(l3)-1])
-
+l1=l+l1
+while(n>1):
+  l=[int(i) for i in input().split()]
+  l1=l+l1
+  n=n-1
+l1=sorted(l1)
+for i in range(0,len(l1)-1):
+  print(l1[i],end=" ")
+print(l1[len(l1)-1])
