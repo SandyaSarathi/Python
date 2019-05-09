5582044f9d6d7a4b39655d0b6bb6313a36302d19
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-10 00:33:44 +0530

    Update pro_40.py

--- pro_40.py
+++ pro_40.py
@@ -2,11 +2,14 @@ s=input()
 s1=list(s)
 t=s1
 l=['d','h','o','n','i']
-for i in range(0,len(s1)):
-  if l[i] in s1:
-    t.remove(l[i])
-    l[i]=0
-if len(l)!=l.count(0):
+if len(s1)>len(l):
   print("no")
 else:
-  print("yes")
+  for i in range(0,len(s1)):
+    if l[i] in s1:
+      t.remove(l[i])
+      l[i]=0
+  if len(l)!=l.count(0):
+    print("no")
+  else:
+    print("yes")
