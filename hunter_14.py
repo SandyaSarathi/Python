ad16f00502aebc0b9fcc1c0cda6927483e4087b1
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-10 01:27:07 +0530

    Update hunter_14.py

--- hunter_14.py
+++ hunter_14.py
@@ -1,13 +1,6 @@
-# your code goes here
+a=input()
 from itertools import permutations
-s=input()
-s1=list(s)
-l=list(permutations(s1))
-l=set(l)
-l=list(l)
-t=""
-for i in range(len(l)):
-	for j in range(len(l[i])):
-		t=t+l[i][j]
-	print(t)
-	t=""
+perm = permutations(a) 
+perm=set(perm)
+for i in list(perm): 
+    print ("".join(i)) 
