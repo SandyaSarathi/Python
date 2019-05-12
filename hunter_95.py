a7e61c4dc6e9bd18a7a9fbfbe6f5e10d6f0e1721
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-12 14:14:09 +0530

    Update hunter_95.py

--- hunter_95.py
+++ hunter_95.py
@@ -1,13 +1,16 @@
 n=int(input())
 l=[]
 f=0
-for i in range(2,n+1):
-    for j in range(2,i+1):
-        if ((i%j)==0) & (i!=j):
-            f=1
-            break
-        else:
-            f=0
-    if f==0:
-        l.append(str(i))
+if n==2:
+    l.append(str(0))
+else:
+    for i in range(2,n+1):
+        for j in range(2,i+1):
+            if ((i%j)==0) & (i!=j):
+                f=1
+                break
+            else:
+                f=0
+        if f==0:
+            l.append(str(i))
 print(" ".join(l))
