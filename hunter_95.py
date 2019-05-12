91bec4063d61fc75ff1931b3c0aa4f6d95707999
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-12 14:15:59 +0530

    Update hunter_95.py

--- hunter_95.py
+++ hunter_95.py
@@ -1,10 +1,10 @@
-n=int(input())
+k=int(input())
 l=[]
 f=0
-if n==2:
+if k==2:
     l.append(str(0))
 else:
-    for i in range(2,n+1):
+    for i in range(2,k+1):
         for j in range(2,i+1):
             if ((i%j)==0) & (i!=j):
                 f=1
