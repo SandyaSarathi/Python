7ed8d049f2d87e1a4fc2e6c1b395cf778eaff4b2
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-13 15:14:39 +0530

    Update 57_pro.py

--- 57_pro.py
+++ 57_pro.py
@@ -1,12 +1,11 @@
-s,t=map(str,input().split())
-t=list(t)
-l=[]
-for i in range(len(s)):
-	if s[i] in t:
-		l.append(s[i])
-		t.remove(s[i])
-if len(l)==0:
-	print("no")
+#san
+a,b=map(str,input().split())
+c=0
+for i in range(0,len(a)):
+    for j in range(0,len(b)):
+        if a[i]==b[j]:
+            c=c+1
+if c>=2:
+    print("yes")
 else:
-	print("yes")
-		
+    print("no")
