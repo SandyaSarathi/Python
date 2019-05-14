76d29fc8f94e34161c4aba681f56343dd8d8e56d
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-14 11:00:52 +0530

    Create hunter_10.py

---
+++ hunter_10.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+l=[int(i) for i in input().split()]
+t=[int(i) for i in input().split()]
+f=1
+for i in range(k):
+	if t[i] in l:
+		f=1
+	else:
+		f=0
+print("YES" if f==1 else "NO")
