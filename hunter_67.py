f3e74f5da9e9a75b6ff116014993c5e40c323860
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-14 11:50:29 +0530

    Update hunter_67.py

--- hunter_67.py
+++ hunter_67.py
@@ -1,4 +1,16 @@
+# coding: utf-8
+# Your code here!
 s=list(input().split("#"))
+s1=[int(i) for i in s if i.isdigit()]
+s1=sum(s1)
 t=list(input().split("#"))
-l=s+t
-print(max(l))
+t1=[int(i) for i in t if i.isdigit()]
+t1=sum(t1)
+d={}
+d[s[0]]=s1
+d[t[0]]=t1
+for i in d:
+    if d[i]==max(s1,t1):
+        print(i)
+
+
