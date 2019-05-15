408287eda1a0e6c51d71471a518fe7b0be7c4ff3
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-15 11:13:57 +0530

    Update player_111.py

--- player_111.py
+++ player_111.py
@@ -2,5 +2,9 @@ n,k=map(int,input().split())
 l=[int(i) for i in input().split()]
 t=l[0:n]
 s=l[n:]
-l=[ i for i in t if i in s]
-print(*set(l))
+l=[]
+for i in t:
+	if i in s:
+		l.append(i)
+		s.remove(i)
+print(*(l))
