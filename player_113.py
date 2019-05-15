75f24c887c1cd477c60c4bcdf6d7c15e9598b00b
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-15 11:41:13 +0530

    Update player_113.py

--- player_113.py
+++ player_113.py
@@ -1,5 +1,7 @@
-s=list(map(int,(input().split("/"))))
-if len(s)!=8:
+s=input()
+n=len(s)
+s=list(map(int,s.split("/")))
+if n!=10:
 	print("no")
 else:
-	print("yes" if s[0]<=31 and s[1]<=12 else "no")
+	print("yes" if ((s[0]<=31) and (s[1]<=12)) else "no")
