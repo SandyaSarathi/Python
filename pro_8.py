129205a67936ce2fb59c848160564d00c2489d72
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-15 22:14:52 -0700

    Update pro_8.py

--- pro_8.py
+++ pro_8.py
@@ -1,8 +1,8 @@
-import gcd from math
-import reduce from functools
+import math
+import functools
 n,k=map(int,input().split())
 l=[int(i) for i in input().split()]
-t=[int(i) for i in input().split()]
-for i in range(l):
-	a,b=t[i][0],t[i][1]
-	g=reduce(gcd,l[a:b])
+for i in range(k):
+    a,b=map(int,input().split())
+    g=functools.reduce(math.gcd,l[a-1:b])
+    print(g)
