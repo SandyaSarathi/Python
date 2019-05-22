43b2c1dfde30cdb0089f60c0661d620315cc6d2c
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-22 10:31:17 +0530

    Update Player_61.py

--- Player_61.py
+++ Player_61.py
@@ -1,7 +1,13 @@
 n,k=map(int,input().split())
 l=list(map(int,input().split()))
+c=0
 for i in range(n):
 	for j in range(i+1,n):
 		if l[i]+l[j]==k:
-			print("yes")
 			break
+		else:
+			c=1
+	if c==0:
+		break
+print("yes" if c==0 else "no")
+	
