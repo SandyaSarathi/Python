511c111d5084d85df22e5b545aacb767ba1264a1
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-23 18:19:30 +0530

    Create Player_162.py

---
+++ Player_162.py
@@ -0,0 +1,17 @@
+n,k=map(int,input().split())
+l=[]
+a=['a','e','i','o','u']
+c=1
+for i in range(n):
+	s=input()
+	l.append(s)
+	if k!=0:
+		for i in s:
+			if i in a:
+				c=1
+				break
+			else:
+				c=0
+		k=k-1
+print("yes" if c==1 else "no")
+	
