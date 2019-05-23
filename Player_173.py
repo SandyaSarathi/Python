62140f24de08c3dfd2a0cb56e7125b0a27070692
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-23 21:37:04 +0530

    Update Player_173.py

--- Player_173.py
+++ Player_173.py
@@ -1,9 +1,22 @@
-s=list(input())
-t=list(input())
-l=[]
-for i in s:
-	if i in t:
-		t.remove(i)
-	else:
-		l.append(i)
-print("".join(l))
+#sa
+s=input()
+r=input()
+g=[]
+if (s.isalpha() or " " in s) and (r.isalpha() or " " in r):
+    s=list(s.split(" "))
+    r=list(r.split(" "))
+    for i in s:
+        if s.count(i) > r.count(i) and i not in g:
+            g.append(i)
+    for i in r:
+        if r.count(i)>s.count(i) and i not in g:
+            g.append(i)
+    print(*g)
+else:
+    for i in s:
+        if s.count(i)>r.count(i) and i not in g:
+            g.append(i)
+    for i in r:
+        if r.count(i)>s.count(i) and i not in g:
+            g.append(i)
+    print(*g)
