b114cdf885bbf8dbc0191edf8dd9f49359b59a17
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-23 15:29:17 +0530

    Create PLayer_142.py

---
+++ PLayer_142.py
@@ -0,0 +1,20 @@
+n,k=map(int,input().split())
+l=[]
+for i in range(n):
+	l.append(input())
+c=0
+for i in range(n-k):
+	t=l[i:k+1]
+	for j in t:
+		if j==l[i]:
+			c=1
+		else:
+			c=0
+			break
+	if c==1:
+		print("yes")
+		break
+if c==0:
+	print("no")
+	
+	
