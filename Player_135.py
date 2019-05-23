324d166166fd7febeef3aec43f85e1cadc7e66f2
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-23 11:42:16 +0530

    Update Player_135.py

--- Player_135.py
+++ Player_135.py
@@ -1,8 +1,14 @@
-#san
-n,l,r=map(int,input().split())
-p=list(map(int,input().split()))
-p.sort()
-for i in p:
-    if i>=l and i<=r:
-        print(i)
-        break
+n=int(input())
+l=list(map(int,input().split()))
+if n%2!=0:
+	m=(n-1)//2
+	a=sorted(l[0:m])
+	b=sorted(l[m:])
+	a=a+b[::-1]
+	print(*a)
+else:
+	m=n//2
+	a=sorted(l[0:m])
+	b=sorted(l[m:])
+	a=a+b[::-1]
+	print(*a)
