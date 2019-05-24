131cdd177e2dd5bf60f3c84e9ae8368d129a2744
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-24 19:15:48 +0530

    Update pro_3.py

--- pro_3.py
+++ pro_3.py
@@ -1,10 +1,10 @@
 s,t=map(str,input().split())
 s=list(s)
 t=list(t)
+m=min(len(s),len(t))
 c=0
-for i in range(len(s)):
+for i in range(m):
 	if s[i]!=t[i]:
 		s[i]=t[i]
 		c+=1
-print(c+len(t)-len(s))
-		
+print(c+abs(len(t)-len(s)))
