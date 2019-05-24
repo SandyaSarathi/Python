80c317fc47a67a5bba8a8d26477a3f6839d3e801
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-24 22:34:40 +0530

    Update pro_24.py

--- pro_24.py
+++ pro_24.py
@@ -1,6 +1,6 @@
 from itertools import product
-n=int(input())
-l=list(product([0,1],repeat=n))
+k=int(input())
+l=list(product([0,1],repeat=k))
 t=[]
 for i in range(len(l)):
 	for j in range(len(l[i])):
