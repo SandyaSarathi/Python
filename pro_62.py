5c593872377b39c557f8a4dbb06526185e00b5a5
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-25 20:33:33 +0530

    Create pro_62.py

---
+++ pro_62.py
@@ -0,0 +1,12 @@
+a = input()
+b = input()
+k = ''
+for i in range(0,len(a)):
+  c = ord(a[i])-96
+  d = ord(b[i])-96
+  if c+d < 26:
+    k = k+chr(ord(chr(c+d+96)))
+  else:
+    k = k+chr(ord(chr(((c+d)-26)+96)))
+print(k)
+#s
