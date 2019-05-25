d308ed8100324ec1a5447b317f28f9f3e6ae45e4
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-25 19:21:38 +0530

    Update pro_36.py

--- pro_36.py
+++ pro_36.py
@@ -1,4 +1,10 @@
 n=int(input())
 l=[int(i) for i in input().split()]
-m=min(l)
-print(l.index(m)+1)
+a=len(l)
+c=0
+for i in range(a):
+	for j in range(i+1,a):
+		for k in range(j+1,a):
+			if l[i]>l[j]>l[k]:
+				c+=1
+print(c)
