4874b8f49939809c3d3424f2697ee4ec81c6290a
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-25 21:26:36 +0530

    Update hunter_21.py

--- hunter_21.py
+++ hunter_21.py
@@ -1,11 +1,19 @@
 n,k=map(int,input().split())
-l=[]
+x=[]
+l1=[]
 for i in range(n):
-    a=[x for x in input().split()]
-    l.append(a)
-for i in range(n):
-	if '0' in l[i]:
-		for j in range(k):
-			l[i][j]=0
-for i in range(n):
-	print(*l[i])
+    l=[int(x) for x in input().split()]
+    x.append(l)
+    if 0 in l:
+        m=l.index(0)
+        l1.append(m)
+for i in range(len(x)):
+    if 0 in x[i]:
+        for j in range(k):
+            x[i][j]=0
+for i in l1:
+    for j in range(n):
+        x[j][i]=0
+for i in x:
+    print(*i)
+#s
