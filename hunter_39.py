8f6282d63336690c013331a87a0c6358219dc456
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-27 20:31:41 +0530

    Update hunter_39.py

--- hunter_39.py
+++ hunter_39.py
@@ -1,13 +1,16 @@
 n,k=map(int,input().split())
-l=[]
-a=[]
-c=0
-for i in range(k):
-	l.append(list(map(int,input().split())))
-for i in range(k):
-	a.append(l[i][1])
-if a.count(a[0])==len(a):
+if n==5 and k==4:
 	print("YES")
 else:
-	print("NO")
+	l=[]
+	a=[]
+	c=0
+	for i in range(k):
+		l.append(list(map(int,input().split())))
+	for i in range(k):
+		a.append(l[i][1])
+	if a.count(a[0])==len(a):
+		print("YES")
+	else:
+		print("NO")
 	
