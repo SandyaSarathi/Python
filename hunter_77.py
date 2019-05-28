b0b14e9ee245d9154c7903e1b454673a733b36ff
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-28 17:17:00 +0530

    Create hunter_77.py

---
+++ hunter_77.py
@@ -0,0 +1,22 @@
+n,k=map(int,input().split())
+l=[]
+for i in range(k):
+	l.append(list(map(int,input().split())))
+s=0
+d={}
+for i in range(len(l)):
+	if l[i][0] in d:
+		d[l[i][0]]+=l[i][1]
+	else:
+		s=s+l[i][1]
+		d[l[i][0]]=s
+		s=0
+l=d.keys()
+c=0
+for i in d:
+	if d[i]==i:
+		print("YES")
+		c=1
+		break
+if c==0:
+	print("NO")
