be1782733a6e2c4292eeafb4170919cecb578510
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-28 14:57:22 +0530

    Update hunter_43.py

--- hunter_43.py
+++ hunter_43.py
@@ -1,11 +1,8 @@
 s=input()
 c=0
-t=""
-for i in range(len(s)-1):
-	if s[i].isdigit() and c==0:
-		t+=s[i]
-		c+=1
-	elif s[i].isdigit() and s[i+1].isdigit():
+t=s[:2]
+for i in range(2,len(s)-1):
+	if s[i].isdigit() and s[i+1].isdigit():
 		t+=s[i-1]*(int(s[i]+s[i+1])-1)
 		if i+1==len(s)-1:
 			c=1
