0c8c4e6c1165de7fe69078efb039f0b1c37098c3
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-28 10:30:19 +0530

    Create player_201.py

---
+++ player_201.py
@@ -0,0 +1,16 @@
+def catalan(n):
+    if n<=1:
+        return 1
+    else:
+        s=0
+        for i in range(n):
+            s=s+catalan(i)*catalan(n-i-1)
+        return s
+n=int(input())
+if n==0:
+    n=1
+c=[]
+for i in range(n):
+    c.append(catalan(i))
+print(*c)
+#san
