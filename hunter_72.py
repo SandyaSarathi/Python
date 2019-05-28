dee6bb2b593da9abaf771bd6f95ef807b75ffee6
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-05-28 15:40:54 +0530

    Update hunter_72.py

--- hunter_72.py
+++ hunter_72.py
@@ -1,4 +1,12 @@
-s=list(input().split())
-for i in range(0,len(s),2):
-	s[i]=s[i][::-1]
-print(" ".join(s))
+s=input()
+if s[-1]==".":
+	s=s[:-1]
+l=list(s.split(" "))
+a=[]
+for i in range(len(l)):
+	if i%2==0:
+		c=l[i]
+		a.append(c[::-1])
+	else:
+		a.append(l[i])
+print(" ".join(a))
